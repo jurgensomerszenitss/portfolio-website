491b307e4335e4a0165a4d036420d1616c548ab2
Jurgen Somers <jurgen.somers@zenitss.eu>
2020-12-16 14:19:21 +0100

    team updates

--- src/components/Team.vue
+++ src/components/Team.vue
@@ -17,15 +17,18 @@
 
         <div class="panel right" v-bind:class="{openLeft : isQuoteJurgenVisible}">
            <div class="photo">
-               <img src="../assets/img/jurgen.jpg" class="photo" @click="toggleQuoteJurgen()">               
+               <img src="../assets/img/jurgen.jpg" class="photo">               
+               <div class="socmed-container">
+                  <a href="https://www.linkedin.com/in/jurgen-somers/" target="_blank"><img src="../assets/img/logo_linkedin.png" class="link"></a>
+                  <a href="https://www.xing.com/profile/Jurgen_Somers" target="_blank"><img src="../assets/img/logo_xing.png" class="link"></a>
+              </div>
             </div>
               <transition name="slide-fade">
                 <div class="content" v-if="isMounted">
                   <div v-bind:class="{inline : isQuoteJurgenVisible}">
                     <h3>Jurgen Somers  <div  @click="toggleQuoteJurgen()"  class="quoteButton" v-bind:class="{quoteButtonHide : isQuoteJurgenVisible}"/></h3>
                     <h4>CEO / founder</h4>
-                    Active in IT since 2001               
-                    <!-- <img src="../assets/img/quote_hor_show.png" class="quote" @click="toggleQuoteJurgen()" v-if="!isQuoteJurgenVisible"> -->
+                    Active in IT since 2001     
                   </div>
                   <div class="quote openLeft" v-if="isQuoteJurgenVisible" >   
                     <p>
@@ -59,32 +62,19 @@
          <div class="panel left"  v-bind:class="{openRight : isQuoteKseniaVisible}">
             <div class="photo">
                 <img src="../assets/img/ksenia.jpg" class="photo">
+                <div class="socmed-container">
+                  <a href="https://www.xing.com/profile/Ksenia_Medova" target="_blank" alt="Xing profile"><img src="../assets/img/logo_xing.png" class="link"></a>             
+                </div>
             </div>
              <transition name="slide-fade">
                 <div class="content"  v-if="isMounted">
                   <div v-bind:class="{inline : isQuoteKseniaVisible}">
                     <h3>Ksenia Medova    <div  @click="toggleQuoteKsenia()"  class="quoteButton" v-bind:class="{quoteButtonHide : isQuoteKseniaVisible}"/></h3>
                     <h4>CCO / Co-founder</h4>
-                    Experienced in sales and management since 2003                 
+                    Experienced in sales and management since 2003                     
                   </div>                    
                   <div class="quote openRight" v-if="isQuoteKseniaVisible" >             
-                    <p>
-                      <img src="../assets/img/quoteopen.png" class="quoteopen">  
-                      I am little
-                    </p>
-
-                    <p>
-                      I like shoes and dresses
-                    </p>
-
-                    <p>
-                      I don't like gru, gru gru
-                    </P>
-
-                    <p>    
-                      In my opinion
-                      <img src="../assets/img/quoteclose.png" class="quoteclose">
-                    </p>
+                    
                   </div>
                 </div>   
              </transition>
@@ -157,37 +147,12 @@ div.inline {
   margin-right: 10px;
   width:calc(50% - 128px);
 }
-   /* 
-div.panel.right {   
-  transition: margin 700ms;  
-}
 
-div.panel.left {   
-    /* transition: margin 700ms; 
-}*/
-/* 
-img.quoteopen {
-  text-align: left;
-  width:24px;
-  height:24px;
-}
-
-img.quoteclose {
-  text-align: right;
-  width:24px;
-  height:24px;
-} */
-/* 
-img.quote {
-  position:relative;
-  top:-8px;
-  left:-5px;
-  width:24px;
-  height:24px;
-  text-align: right;
-  cursor: pointer;
-} */
-
+div.socmed-container {
+  position:absolute; 
+  bottom:-5px;
+  right:5px
+} 
 div.quoteButton {
   background-image: url("../assets/img/quote_hor_show.png");
   background-repeat:no-repeat;
@@ -232,6 +197,12 @@ div.quoteButtonHide {
     padding-left:0px; 
     padding-top:5px
   } 
+
+  div.socmed-container {
+  position:absolute; 
+  bottom:0px;
+  right:calc(50% - 64px);
+}
  
   p {
       line-height: 10pt;
